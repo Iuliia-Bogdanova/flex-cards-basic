bf243c21dcec45d70e558fdccb11d7c82524265f
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2022-10-09 22:35:43 +0400

    did task 2

---
+++ index.js
@@ -0,0 +1,19 @@
+let a = +prompt(`First number`); 
+
+let b = +prompt(`Second number`);
+
+function calcAdd() {
+    alert(`Result ${a + b}`);
+}
+
+function calcSub() {
+    alert(`Result ${a - b}`);
+}
+
+function calcMult() {
+    alert(`Result ${a * b}`);
+}
+
+function calcDiv() {
+    alert(`Result ${a / b}`);
+}
