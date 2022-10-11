4ba382022431d218c6987fb228b17166b8e8b56c
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2022-10-11 19:42:01 +0400

    changed to a local function

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-let a = +prompt(`First number`); 
-
-let b = +prompt(`Second number`);
-
 function calcAdd() {
+    let a = +prompt(`First number`); 
+    let b = +prompt(`Second number`);
+    
     alert(`Result ${a + b}`);
 }
 
 function calcSub() {
+    let a = +prompt(`First number`); 
+    let b = +prompt(`Second number`);
+
     alert(`Result ${a - b}`);
 }
 
 function calcMult() {
+    let a = +prompt(`First number`); 
+    let b = +prompt(`Second number`);
+
     alert(`Result ${a * b}`);
 }
 
 function calcDiv() {
+    let a = +prompt(`First number`); 
+    let b = +prompt(`Second number`);
+
     alert(`Result ${a / b}`);
 }
